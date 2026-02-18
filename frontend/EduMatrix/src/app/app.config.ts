0037877732a5d358f4cb65b2e641609d56986406
n-szilard <nagyapatiszilard@turr.hu>
2026-02-18 10:30:26 +0100

    Csomagok, betűtípus, alapvető komponensek

--- frontend/EduMatrix/src/app/app.config.ts
+++ frontend/EduMatrix/src/app/app.config.ts
@@ -3,6 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+  ]
 };
