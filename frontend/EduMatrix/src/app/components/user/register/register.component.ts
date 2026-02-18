d8f62339c24532cf83680293a10378d4865d2223
Andor1298 <palinkasandor@turr.hu>
2026-02-18 12:28:34 +0100

    login es register oldal

--- frontend/EduMatrix/src/app/components/user/register/register.component.ts
+++ frontend/EduMatrix/src/app/components/user/register/register.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+  if (password && confirmPassword && password.value !== confirmPassword.value) {
+    return { passwordMismatch: true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    InputTextModule,
+    PasswordModule,
+    CheckboxModule,
+    DropdownModule,
+    ButtonModule,
+    CardModule
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  registerForm!: FormGroup;
+
+  roles = [
+    { label: 'Adminisztrátor', value: 'admin' },
+    { label: 'Tanár', value: 'teacher' },
+    { label: 'Tanuló', value: 'student' },
+    { label: 'Szülő', value: 'parent' }
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+      role: ['', Validators.required],
+      acceptTerms: [false, Validators.requiredTrue]
+    }, { validators: passwordMatchValidator });
+  }
+
+  register(): void {
+    if (this.registerForm.valid) {
+      console.log('Register form submitted:', this.registerForm.value);
+      // TODO: service hívás
+    }
+  }
 }
