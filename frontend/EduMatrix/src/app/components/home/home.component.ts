5fe3895f1831a21903f643f3faab5162842c4a25
n-szilard <nagyapatiszilard@turr.hu>
2026-02-18 12:28:07 +0100

    Home oldal importok

--- frontend/EduMatrix/src/app/components/home/home.component.ts
+++ frontend/EduMatrix/src/app/components/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonModule, CardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
