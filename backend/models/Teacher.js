b67914d539407411669fe8d544b7fd2f1ed1412c
n-szilard <nagyapatiszilard@turr.hu>
2026-02-26 13:14:31 +0100

    EP-25 Teachers tábla

---
+++ backend/models/Teacher.js
@@ -0,0 +1,42 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+const UUID = DataTypes.UUID;
+
+module.exports = (sequelize) => {
+  class Teacher extends Model {
+    static associate(models) {
+      Teacher.belongsTo(models.User, { foreignKey: 'user_id' });
+      Teacher.hasMany(models.Note, { foreignKey: 'teacher_id' });
+      Teacher.hasMany(models.ClassSubject, { foreignKey: 'teacher_id' });
+    }
+  }
+
+  Teacher.init(
+    {
+      id: {
+        type: UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      user_id: {
+        type: UUID,
+        allowNull: false,
+        unique: true,
+        references: { model: 'users', key: 'id' },
+      },
+      full_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Teacher',
+      tableName: 'teachers',
+      timestamps: false,
+    }
+  );
+
+  return Teacher;
+};
