7981977939cbdd2c0b863e218923356d774e21b0
n-szilard <nagyapatiszilard@turr.hu>
2026-02-26 13:33:14 +0100

    EP-31 Absence, Grade, Note és Timetable tábla, és tábla bekötések

--- backend/models/index.js
+++ backend/models/index.js
@@ -17,9 +17,20 @@ const sequelize = new Sequelize(
 // Modellek betöltése
 const Role = require('./Role')(sequelize);
 const User = require('./User')(sequelize);
+const Class = require('./Class')(sequelize);
+const Subject = require('./Subject')(sequelize);
+const Teacher = require('./Teacher')(sequelize);
+const Student = require('./Student')(sequelize);
+const ClassSubject = require('./ClassSubject')(sequelize);
+const Note = require('./Note')(sequelize);
+const Absence = require('./Absence')(sequelize);
+const Grade = require('./Grade')(sequelize);
+const Timetable = require('./Timetable')(sequelize);
 
-const models = { Role, User };
-console.log(models);
+const models = {
+    Role, User, Class, Subject, Teacher,
+    Student, ClassSubject, Note, Absence, Grade, Timetable,
+};
 
 Object.values(models).forEach((model) => {
     if (typeof model.associate === 'function') {
