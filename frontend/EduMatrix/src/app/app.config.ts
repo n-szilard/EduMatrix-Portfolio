c2fc8263dfa46f651adfbbd22da94c7ae430698d
n-szilard <nagyapatiszilard@turr.hu>
2026-03-03 13:14:50 +0100

    Belépés és Regisztráció adat validálás

--- frontend/EduMatrix/src/app/app.config.ts
+++ frontend/EduMatrix/src/app/app.config.ts
@@ -3,11 +3,18 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import EduMatrixPreset from './edumatrix-theme'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(),
+    providePrimeNG({
+      theme: {
+        preset: EduMatrixPreset
+      }
+    })
   ]
 };
